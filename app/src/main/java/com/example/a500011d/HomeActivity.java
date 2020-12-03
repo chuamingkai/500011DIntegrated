f01aa2f0b5bccd6ca373e18bcd1c4b30eb16cf62
chuamingkai <chuamingkai@gmail.com>
2020-12-03 17:32:11 +0800

    Change xml names

--- app/src/main/java/com/example/a500011d/HomeActivity.java
+++ app/src/main/java/com/example/a500011d/HomeActivity.java
@@ -22,7 +22,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
     Button chat;
@@ -37,7 +36,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         chat = findViewById(R.id.ChatButton);
         post = findViewById(R.id.PostButton);
         text = findViewById(R.id.ITEMLIST);
