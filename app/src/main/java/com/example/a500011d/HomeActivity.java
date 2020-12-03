e0b43b6e19b68cda83196efdd531876f0bf6c152
chuamingkai <chuamingkai@gmail.com>
2020-12-03 21:40:46 +0800

    Update datetime formatting to English dates only Update get username for PostingActivity Update activity_login.xml to hide password input

--- app/src/main/java/com/example/a500011d/HomeActivity.java
+++ app/src/main/java/com/example/a500011d/HomeActivity.java
@@ -14,12 +14,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -27,16 +32,49 @@ public class HomeActivity extends AppCompatActivity {
     Button chat;
     Button post;
     TextView text;
+    String username;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private DocumentReference docRef = db.collection("Items").document("LostItem");
     public Map<String,Object> doc;
+
     DatabaseReference mRootDatabaseRef;
     DatabaseReference mNodeRefItem;
+    ArrayList<ItemEntry> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Intent intent = getIntent();
+        username = intent.getStringExtra(getString(R.string.username_intent));
+
+        // Here is code to get ItemEntry in ArrayList from Firebase database
+        mRootDatabaseRef = FirebaseDatabase.getInstance().getReference();
+        mNodeRefItem = mRootDatabaseRef.child(getString(R.string.item_node_key));
+        mNodeRefItem.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                long count = snapshot.getChildrenCount();
+                if (count > 0) {
+                    data = new ArrayList<ItemEntry>((int) count);
+                    for (Iterator<DataSnapshot> item = snapshot.getChildren().iterator(); item.hasNext(); ) {
+                        DataSnapshot ds = item.next();
+                        ItemEntry entry = ds.getValue(ItemEntry.class);
+                        entry.databaseId = ds.getKey();
+                        data.add(entry);
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
         chat = findViewById(R.id.ChatButton);
         post = findViewById(R.id.PostButton);
         text = findViewById(R.id.ITEMLIST);
@@ -91,6 +129,7 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(HomeActivity.this,
                         PostingActivity.class);
+                intent.putExtra(getString(R.string.username_intent), username);
                 startActivity(intent);
             }
         });
